42f41e58b51fd86e983a67823329deae2ca65803
Ninux Ad <ninux.ad@gmail.com>
2017-12-20 19:56:05 -0800

    Go path issues

--- go1.go
+++ go1.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	list := []string{'Hello', 'How', 'Are', 'You'}
-	list = append(list, 'Folks')
-	list = append(list, 'Doing')
+	list := []string{"Hello", "How", "Are", "You"}
+	list = append(list, "Folks")
+	list = append(list, "Doing")
 	for index, element := range list {
 		fmt.Println(index, element)
-	} 
+	}
 }
