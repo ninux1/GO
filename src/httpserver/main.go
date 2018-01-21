98752791963f223c1019061a4bb199fb0bcfbbf0
Ninux Ad <ninux.ad@gmail.com>
2018-01-21 01:16:04 -0800

    http flags

---
+++ src/httpserver/main.go
@@ -0,0 +1,14 @@
+package main
+
+
+func main() {
+	serve()
+	/*
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Directory not found")
+	}
+
+	http.ListenAndServe(":3000", http.FileServer(http.Dir(dir)))
+	*/
+}
